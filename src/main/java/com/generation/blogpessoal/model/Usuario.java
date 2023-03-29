ca29db83af31860245438ac7c8295295f4926211
Tay Teixeira <taymaratda@gmail.com>
2023-03-29 19:22:07 -0300

    Implementação Testes JUnit - Back end Blog Pessoal

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -41,6 +41,18 @@ public class Usuario {
 	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
+	
+	/* Métodos Construtores  */
+
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	public Usuario() { }
 
 	/* Insira os Getters and Setters */
 
